6d68f50aa70691a7a09019bacc70806b6f431f8d
Naresh Sadineni <naresh_sadineni@hotmail.com>
2017-06-07 22:03:20 -0400

    added new Files and two way binding

---
+++ app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>ID: {{hero.id}} </label> </div>
+        <div><label> Name: </label> <input [(ngModel)]= "hero.name" placeholder="name"/> </div>`
+
+    //< ul >
+    //<li *ngFor="let item in heroes">
+    //<label>{{item.id }}</label>
+    //< label > {{item.name }}</label>
+    //< !--each hero goes here -- >
+    //</li>
+    //< /ul>
+})
+
+export class AppComponent {
+    title = 'Tour of Heroes';
+    hero: Hero = {
+        id: 1,
+        name: "Windstorm"
+    };
+
+    Heroes: Hero[] = [
+        { id: 11, name: 'Mr. Nice' },
+        { id: 12, name: 'Narco' },
+        { id: 13, name: 'Bombasto' },
+        { id: 14, name: 'Celeritas' },
+        { id: 15, name: 'Magneta' },
+        { id: 16, name: 'RubberMan' },
+        { id: 17, name: 'Dynama' },
+        { id: 18, name: 'Dr IQ' },
+        { id: 19, name: 'Magma' },
+        { id: 20, name: 'Tornado' }
+    ];
+
+    heroes = this.Heroes;
+
+    //ngOnInit() {
+    //    this.getData();
+    //}
+
+    //public getData() {
+    //    this.http.get("http://webapiwithazuredatabase20170605114137.azurewebsites.net/api/values/1")
+    //        .map(result => JSON.parse(result.json()));
+    //}
+}
+
+export class Hero {
+    id: number;
+    name: string;
+}
