9c9d200819e148b419beedc8003402ef24943a45
Naresh Sadineni <naresh_sadineni@hotmail.com>
2017-06-08 15:27:44 -0400

    main.js

---
+++ Events/events-component.js
@@ -0,0 +1,8 @@
+"use strict";
+var eventsComponent = (function () {
+    function eventsComponent() {
+    }
+    return eventsComponent;
+}());
+exports.eventsComponent = eventsComponent;
+//# sourceMappingURL=events-component.js.map
