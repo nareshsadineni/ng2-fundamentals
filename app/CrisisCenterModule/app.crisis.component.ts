bd2f7673eed4bab5569b864e5d225ed5d50a983b
Naresh Sadineni <naresh_sadineni@hotmail.com>
2017-06-14 11:48:36 -0400

    Added Routing to start implementing navigation Moved showing heroes list to a new seperate component Created New Component called Crisis Added Not Found Component for handling unknown requests Created Service to serve the data inorder to avoid duplication of code and as a best practice

---
+++ app/CrisisCenterModule/app.crisis.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector: 'crisis-center',
+    template:    `    <h1>Crisis</h1>    `
+    
+})
+
+export class CrisisComponent{
+
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { PageNotFoundComponent } from './PageNotFoundModule/app.not-found.component';
+import { CrisisComponent } from './CrisisCenterModule/app.crisis.component'
+
+const appRoutes: Routes = [
+    { path: 'crisis-center', component: CrisisComponent },
+    { path: 'hero/:id', component: HeroDetailComponent },
+    {
+        path: 'heroes',
+        component: HeroesComponent,
+        data: { title: 'Heroes List' }
+    },
+    {
+        path: '',
+        redirectTo: '/heroes',
+        pathMatch: 'full'
+    },
+    { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
     imports: [
-        BrowserModule
-        ,FormsModule
+        RouterModule.forRoot(appRoutes),
+        BrowserModule,
+        FormsModule
     ],
     declarations: [
         AppComponent,
-        HeroDetailComponent
+        HeroDetailComponent,
+        HeroesComponent,
+        PageNotFoundComponent,
+        CrisisComponent
     ],
     bootstrap: [AppComponent]
 })
